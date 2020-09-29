0a13bf4a5874f265d880e4bb21f78aa2dc66d99e
Abhishek <abhishekcs26@gmail.com>
2020-09-29 23:59:29 +0530

    version update 0.1.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     setup(
         name="video2images",
         scripts=["scripts/video2images"],
-        version="0.0.7",
+        version="0.1.0",
         description="Video 2 Image converter",
         long_description=long_description,
         long_description_content_type="text/markdown",
