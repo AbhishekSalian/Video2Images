e5b171dc6a8dcbb0f457b5ac4d419b6d141a7377
Abhishek C. Salian <43337429+AbhishekSalian@users.noreply.github.com>
2020-09-29 17:17:37 +0530

    # inital commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Video2Images
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+<p align="center">
+<img src="./image/logo.png"></a>
+</p>
+
+
+## About
+A library which helps to convert volumetric video data into image frames with adjustable frame capturing duration functionality for video data annotation.
+
+The main reason why I created this library is ease of video to image frames conversion. During video annotation we need individual frames of an video or a subpart of a video and that to tunable frame rate so this problems led to this library which easily helps you to do this task.
+
+## How to install?
+```pip install video2images```
+
+
+## Requirements
+- pip install tqdm
+- pip install imageio
+- pip install imageio-ffmpeg
+- pip install moviepy
+
+
+##
+
+
+
---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, Extension
+from setuptools import find_packages
+
+
+with open("README.md") as f:
+    long_description = f.read()
+
+classifiers = [
+               "Programming Language :: Python :: 3",
+               "License :: OSI Approved :: MIT License",
+               "Operating System :: OS Independent",
+               "Development Status :: 4 - Beta",
+               "Intended Audience :: Science/Research",
+               "Intended Audience :: Financial and Insurance Industry",
+               "Intended Audience :: Developers",
+               "Topic :: Software Development :: Libraries :: Python Modules",
+               "Natural Language :: English"
+              ],
+
+SHORT_DESC = "This is a random word generator module"
+
+requirements = ["tqdm",
+                "imageio",
+                "moviepy",
+                "imageio-ffmpeg"]
+
+
+if __name__ == "__main__":
+    setup(
+        name="VIDEO2IMAGES",
+        scripts=["scripts/video2images"],
+        version="0.0.1",
+        description="Video 2 Image converter",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="Abhishek C. Salian",
+        author_email="abhishek.c.salian@gmail.com",
+        url="",
+        license="MIT License",
+        packages=find_packages(exclude=["*tests*"]),
+        package_data={'': ['LICENSE', 'README.md']},
+        include_package_data=True,
+        install_requires=requirements,
+        package=["video2images"],
+        platforms=["windows", "linux", "unix"],
+        python_requires=">=3.5"
+    )
